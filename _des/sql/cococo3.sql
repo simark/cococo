c650bd7386f391e939ef6448f3f63cad3bca6dff
Simon Marchi <thundersim@gmail.com>
2012-06-04 23:49:50 -0700

    Updated translation

--- _des/sql/cococo3.sql
+++ _des/sql/cococo3.sql
@@ -1,17 +1,403 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.10
+-- version 3.4.11deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 04, 2012 at 06:18 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Generation Time: Jun 05, 2012 at 06:48 AM
+-- Server version: 5.5.23
+-- PHP Version: 5.4.0-3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `cococo`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_debt`(
+	IN p_id_user INT,
+	IN p_other_user TEXT,
+	IN p_descr TEXT,
+	IN p_amount INT,
+	IN p_creator TEXT,
+	IN p_dir VARCHAR(16),
+	IN p_is_payback BOOL,
+	IN p_date_real DATE	
+)
+BEGIN
+	DECLARE id_other_user INT;
+	DECLARE id_user_src_v INT;
+	DECLARE id_user_dst_v INT;
+	SELECT
+		id
+	INTO
+		id_other_user
+	FROM
+		users
+	WHERE
+		username = p_other_user OR
+		email = p_other_user OR
+		id = p_other_user;
+	IF (ISNULL(id_other_user)) THEN
+		SELECT 2 AS res;
+	ELSE
+		IF (id_other_user = p_id_user) THEN
+			SELECT 3 AS res;
+		ELSE
+			IF (p_dir = 'iowethem') THEN
+				SET id_user_src_v = p_id_user;
+				SET id_user_dst_v = id_other_user;
+			ELSE
+				SET id_user_src_v = id_other_user;
+				SET id_user_dst_v = p_id_user;
+			END IF;
+			INSERT INTO
+				debts
+			(
+				id_user_src,
+				id_user_dst,
+				creator,
+				amount,
+				descr,
+				is_payback,
+				is_confirmed,
+				date_real,
+				date_creation
+			)
+			VALUES(
+				id_user_src_v,
+				id_user_dst_v,
+				p_creator,
+				p_amount,
+				p_descr,
+				p_is_payback,
+				FALSE,
+				p_date_real,
+				NOW()
+			);
+			SELECT 1 AS res;
+		END IF;
+	END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_user`(
+	IN p_first_name TEXT,
+	IN p_last_name TEXT,
+	IN p_email TEXT,
+	IN p_birthday DATE,
+	IN p_gender VARCHAR(16),
+	IN p_id_locale INT,
+	IN p_username VARCHAR(30),
+	IN p_sha1_passwd VARCHAR(40)
+)
+BEGIN
+	DECLARE id INT;
+	INSERT INTO
+		users
+	(
+		first_name,
+		last_name,
+		email,
+		birthday,
+		gender,
+		id_locale,
+		username,
+		passwd,
+		is_active,
+		date_creation,
+		theme
+	)
+	VALUES(
+		p_first_name,
+		p_last_name,
+		p_email,
+		p_birthday,
+		p_gender,
+		p_id_locale,
+		p_username,
+		p_sha1_passwd,
+		TRUE,
+		NOW(),
+		'leaves'
+	);
+	SELECT
+		LAST_INSERT_ID()
+	INTO
+		id;
+	IF (id > 0) THEN
+		INSERT INTO
+			user_groups
+		(
+			id_user,
+			id_group
+		)
+		VALUES(
+			id,
+			2
+		);
+	END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_user_fav`(
+	IN p_id_user INT,
+	IN p_fav_username TEXT
+)
+BEGIN
+	DECLARE id_other_user INT;
+	SELECT
+		id
+	INTO
+		id_other_user
+	FROM
+		users
+	WHERE
+		username = p_fav_username;
+	IF (ISNULL(id_other_user)) THEN
+		SELECT 2 AS res;
+	ELSE
+		IF (id_other_user = p_id_user) THEN
+			SELECT 3 AS res;
+		ELSE
+			INSERT INTO
+				favs
+			(
+				id_user,
+				id_user_fav
+			)
+			VALUES(
+				p_id_user,
+				id_other_user
+			);
+			SELECT 1 AS res;
+		END IF;
+	END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `assign_user_to_group`(
+	IN p_id_user INT,
+	IN p_id_group INT
+)
+BEGIN
+	INSERT INTO
+		user_groups
+	(
+		id_user,
+		id_group
+	)
+	VALUES(
+		p_id_user,
+		p_id_group
+	);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `confirm_debt`(
+	IN p_id_user INT,
+	IN p_id_debt INT
+)
+BEGIN
+	DECLARE z BOOLEAN;
+	SELECT
+		(id_user_src = p_id_user AND creator <> 'src' OR
+		id_user_dst = p_id_user AND creator <> 'dst')
+	INTO
+		z
+	FROM
+		debts
+	WHERE
+		id = p_id_debt AND
+		is_confirmed = FALSE;
+	IF (z = TRUE) THEN
+		UPDATE
+			debts
+		SET
+			is_confirmed = TRUE
+		WHERE
+			id = p_id_debt;
+		SELECT 1 AS res;
+	ELSE
+		SELECT 0 AS res;
+	END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_all_user_favs`(
+	IN p_id_user INT,
+	IN p_id_user_fav INT
+)
+BEGIN
+	DELETE FROM
+		favs
+	WHERE
+		id_user = p_id_user;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user`(
+	IN p_id_user INT
+)
+BEGIN
+	DELETE FROM
+		debts
+	WHERE
+		id_user_src = p_id_user OR
+		id_user_dst = p_id_user;
+	DELETE FROM
+		user_groups
+	WHERE
+		id_user = p_id_user;
+	DELETE FROM
+		users
+	WHERE
+		id = p_id_user;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_fav`(
+	IN p_id_user INT,
+	IN p_id_user_fav INT
+)
+BEGIN
+	DELETE FROM
+		favs
+	WHERE
+		id_user = p_id_user AND
+		id_user_fav = p_id_user_fav;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_locale_string_for_code`(
+	IN p_strkey VARCHAR(32),
+	IN p_code VARCHAR(8)
+)
+BEGIN
+	SELECT
+		locales_strings.str AS res
+	FROM
+		locales
+		LEFT JOIN locales_strings ON (locales.id = locales_strings.id_locale)
+	WHERE
+		locales.lang = p_code AND
+		locales_strings.strkey = p_strkey;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_locale_string_for_user`(
+	IN p_strkey VARCHAR(32),
+	IN p_id_user INT
+)
+BEGIN
+	SELECT
+		locales_strings.str AS res
+	FROM
+		users
+		LEFT JOIN locales_strings ON (users.id_locale = locales_strings.id_locale)
+	WHERE
+		users.id = p_id_user AND
+		locales_strings.strkey = p_strkey;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `invalidate_debt`(
+	IN p_id_user INT,
+	IN p_id_debt INT
+)
+BEGIN
+	DECLARE z BOOLEAN;
+	SELECT
+		(id_user_src = p_id_user AND creator <> 'src' OR
+		id_user_dst = p_id_user AND creator <> 'dst')
+	INTO
+		z
+	FROM
+		debts
+	WHERE
+		id = p_id_debt AND
+		is_confirmed = FALSE;
+	IF (z = TRUE) THEN
+		DELETE FROM
+			debts
+		WHERE
+			id = p_id_debt;
+		SELECT 1 AS res;
+	ELSE
+		SELECT 0 AS res;
+	END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `unassign_user_from_group`(
+	IN p_id_user INT,
+	IN p_id_group INT
+)
+BEGIN
+	DELETE FROM
+		user_groups
+	WHERE
+		id_user = p_id_user AND
+		id_group = p_id_group;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_user`(
+	IN p_id_user INT,
+	IN p_first_name TEXT,
+	IN p_last_name TEXT,
+	IN p_email TEXT,
+	IN p_birthday DATE,
+	IN p_gender VARCHAR(16),
+	IN p_username VARCHAR(30),
+	IN p_sha1_passwd VARCHAR(40),
+	IN p_update_passwd BOOLEAN,
+	IN p_theme VARCHAR(32)
+)
+BEGIN
+	UPDATE
+		users
+	SET
+		first_name = p_first_name,
+		last_name = p_last_name,
+		email = p_email,
+		birthday = p_birthday,
+		gender = p_gender,
+		username = p_username,
+		passwd = IF(
+			p_update_passwd,
+			p_sha1_passwd,
+			passwd
+		),
+		theme = p_theme
+	WHERE
+		id = p_id_user;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_user_active`(
+	IN p_id_user INT,
+	IN p_state BOOLEAN
+)
+BEGIN
+	UPDATE
+		users
+	SET
+		is_active = p_state
+	WHERE
+		id = p_id_user;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_user_admin`(
+	IN p_id_user INT,
+	IN p_state BOOLEAN
+)
+BEGIN
+	IF (p_state = TRUE) THEN
+		CALL assign_user_to_group(p_id_user, 3);
+	ELSE
+		CALL unassign_user_from_group(p_id_user, 3);
+	END IF;
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -68,7 +454,7 @@ CREATE TABLE IF NOT EXISTS `features` (
 INSERT INTO `features` (`id`, `name`, `descr`) VALUES
 (1, 'ADD_DEBT', 'ajouter une dette'),
 (2, 'CLEAR_DEBT', 'rembourser une dette'),
-(3, 'SETTINGS', 'modifier ses paramËtres'),
+(3, 'SETTINGS', 'modifier ses param'),
 (4, 'GET_MY_SUMMARY', 'obtenir mon sommaire'),
 (6, 'GET_MY_HISTORY', 'obtenir mon historique'),
 (7, 'ADD_DEBT', 'ajouter une dette'),
@@ -79,7 +465,7 @@ INSERT INTO `features` (`id`, `name`, `descr`) VALUES
 (12, 'GET_USERS', 'obtenir les utilisateurs'),
 (13, 'SEARCH_USER', 'rechercher les utilisateurs'),
 (14, 'GET_FAVS', 'obtenir mes favoris'),
-(15, 'ADD_TO_MY_FAVS', 'ajouter un utilisateur ‡ mes favoris'),
+(15, 'ADD_TO_MY_FAVS', 'ajouter un utilisateur '),
 (16, 'MODIFY_PROFILE', 'modifier son profil'),
 (17, 'GET_ZUSER', 'obtenir un zuser'),
 (18, 'REMOVE_FROM_MY_FAVS', 'retirer de mes favoris');
@@ -174,7 +560,7 @@ CREATE TABLE IF NOT EXISTS `locales` (
 --
 
 INSERT INTO `locales` (`id`, `lang`, `name`) VALUES
-(1, 'fr', 'franÁais'),
+(1, 'fr', 'fran'),
 (2, 'en', 'english'),
 (3, 'de', 'deutsch');
 
@@ -192,8 +578,31 @@ CREATE TABLE IF NOT EXISTS `locales_strings` (
   PRIMARY KEY (`id`),
   KEY `fk_locale` (`id_locale`),
   KEY `strkey` (`strkey`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
 
+--
+-- Dumping data for table `locales_strings`
+--
+
+INSERT INTO `locales_strings` (`id`, `id_locale`, `strkey`, `str`) VALUES
+(5, 2, 'logout', 'Logout'),
+(6, 1, 'logout', 'D√©connexion'),
+(7, 1, 'my-cococo', 'mon cococo'),
+(8, 2, 'my-cococo', 'my cococo'),
+(9, 2, 'summary', 'summary'),
+(10, 2, 'totals', 'totals'),
+(11, 2, 'history', 'history'),
+(12, 1, 'totals', 'totaux'),
+(13, 1, 'history', 'historique'),
+(14, 1, 'summary', 'sommaire'),
+(15, 2, 'add-debt', 'add a debt'),
+(16, 1, 'add-debt', 'ajouter une dette'),
+(17, 2, 'add-debt-desc', 'This form allows you to add a debt. After you submitted the debt, the other concerned user will have to confirm the new debt.'),
+(18, 1, 'add-debt-desc', 'Ce formulaire vous permet d''ajouter une dette. √Ä la suite de la soumission, l''autre utilisateur devra confirmer la dette ajout√©e afin que celle-ci soit officialis√©e dans le syst√®me.'),
+(19, 2, 'hide-show-favs', 'hide/show favorites'),
+(20, 1, 'hide-show-favs', 'montrer/masquer mes favoris'),
+(21, 2, 'other-user', 'other user'),
+(22, 1, 'other-user', 'autre utilisateur');
 
 -- --------------------------------------------------------
 
@@ -218,7 +627,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   KEY `fk_users_locales` (`id_locale`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=27 ;
 
 -- --------------------------------------------------------
 
@@ -375,3 +784,7 @@ ALTER TABLE `users`
 ALTER TABLE `user_groups`
   ADD CONSTRAINT `user_groups_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `user_groups_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `groups` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

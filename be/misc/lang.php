a9b0f52a8c277087c691e4ba7706aea5bc11c537
Simon Marchi <thundersim@gmail.com>
2012-06-05 21:14:24 -0700

    Load locale strings from a file. Updated translations.

---
+++ be/misc/lang.php
@@ -0,0 +1,40 @@
+<?php
+
+$fr = array(
+	'username' => 'utilisateur',
+	'password' => 'mot de passe',
+);
+
+$en = array(
+	'username' => 'username',
+	'password' => 'password',
+	'go_login' => 'login',
+	'welcome' => 'welcome on cococo !',
+	'logout' => 'logout',
+	'my-cococo' => 'my cococo',
+	'totals' => 'totals',
+	'history' => 'history',
+	'summary' => 'summary',
+	'add-debt' => 'add debt',
+	'add-debt-desc' => 'This form allows you to add a debt. After you submitted the debt, the other concerned user will have to confirm the new debt.',
+	'hide-show-favs' => 'hide / show favorites',
+	'other-person' => 'other person',
+	'amount' => 'amount',
+	'refund' => 'refund',
+	'it-is-a-refund' => 'it is a refund',
+	'which-way' => 'which way ?',
+	'i-owe-them' => 'i owe them',
+	'they-owe-me' => 'they-owe-me',
+	'description' => 'description',
+	'optional' => 'optional',
+	'debt-date' => 'date of the debt',
+	'date-format-desc' => 'date format: YYYY-MM-DD',
+	
+);
+
+$locales_strings = array(
+	'en' => $en,
+	'fr' => $fr,
+);
+
+?>

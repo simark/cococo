0e7e6986221508418ff85f0bdb719186c95b2a01
Simon Marchi <thundersim@gmail.com>
2012-06-12 10:25:04 -0700

    Basic value replacement in lang strings

--- lang/lang.php
+++ lang/lang.php
@@ -14,8 +14,8 @@
 	 *
 	 * @param string $key	Translation key
 	 */
-	function T($key) {
-    echo _T($key);
+	function T($key, $values = array()) {
+    echo _T($key, $values);
 	}
 	
 	/**
@@ -26,8 +26,8 @@
 	 * @param string $key	Translation key
 	 * @return string	Translated string
 	 */
-	function _T($key) {
-    return hs(_T_($key));
+	function _T($key, $values = array()) {
+    return hs(_T_($key, $values));
 	}
 	
 	/**
@@ -35,8 +35,8 @@
 	 *
 	 * @param string $key	Translation key
 	 */
-	function T_($key) {
-		echo _T_($key);
+	function T_($key, $values = array()) {
+		echo _T_($key, $values);
 	}
 	
 	/**
@@ -47,13 +47,61 @@
 	 * @param string $key	Translation key
 	 * @return string	Translated string
 	 */
-	function _T_($key) {
+	function _T_($key, $values = array()) {
 		global $g_locales_strings, $g_locale;
 
 		if (isset($g_locales_strings[$key][$g_locale]) && $g_locales_strings[$key][$g_locale]) {
-		  return $g_locales_strings[$key][$g_locale];
+      $str = $g_locales_strings[$key][$g_locale];
+		  return replace_values($str, $values);
 		} else {
 		  err_ls_not_found($key, $g_locale);
 		}
 	}
+
+  /**
+   *
+   * @param string The template string
+   * @param values Key-values to replace.
+   */
+  function replace_values($string, $values) {
+    $search_offset = 0;
+
+    $pattern1 = '/\\{\\s*([a-zA-Z0-9_]+)\\s*\\}/';
+    $pattern2 = '/\\{\\s*([a-zA-Z0-9_]+)\\s*\\|\\s*([a-zA-Z0-9_]+)\\s*\\}/';
+    
+    while (preg_match($pattern1, $string, $matches, PREG_OFFSET_CAPTURE, $search_offset) ||
+           preg_match($pattern2, $string, $matches, PREG_OFFSET_CAPTURE, $search_offset)) {
+      /* Get full match */
+      $matched_str_arr = array_shift($matches);
+      $matched_str_offset = $matched_str_arr[1];
+      $matched_str = $matched_str_arr[0];
+      $matched_str_len = strlen($matched_str);
+
+      /* By default, if we don't replace, this will be the new search offset */
+      $search_offset = $matched_str_offset + $matched_str_len;
+
+      /* Get parts matches */
+      $key = $matches[0][0];
+      $filter = isset($matches[1]) ? $matches[1][0] : null;
+
+      if (!isset($values[$key])) {
+        continue;
+      }
+
+      $replacement_str = $values[$key];
+
+      if ($filter) {
+        // TODO Replace by actual filter call
+        $replacement_str = $replacement_str . $replacement_str;
+      }
+      
+      $replacement_len = strlen($replacement_str);
+      $string = substr_replace($string, $replacement_str, $matched_str_offset, $matched_str_len);
+
+      /* Start of the new search */
+      $search_offset = $matched_str_offset + $replacement_len;
+    }
+
+    return $string;
+  }
 ?>
